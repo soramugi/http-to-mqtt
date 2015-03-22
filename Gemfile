7f92273c7bc208168ba7742deecdfc812ebc8de2
soramugi <soramugi.chika@gmail.com>
2015-03-23 00:58:13 +0900

    first commit

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source "https://rubygems.org"
+
+gem "sinatra"
+gem "dotenv"
+gem "mqtt", git: "https://github.com/njh/ruby-mqtt.git"
---
+++ app.rb
@@ -0,0 +1,25 @@
+require 'bundler'
+Bundler.require
+
+set server: 'webrick'
+Dotenv.load
+
+def auth?(user, pass)
+  (ENV['AUTH_USER'].nil? || user == ENV['AUTH_USER']) && pass == ENV['AUTH_PASS']
+end
+
+get '/:path' do
+  return status 400 unless auth?(params[:username], params[:path])
+
+  message = ENV['MQTT_MESSAGE'] || params[:message] || 1
+  topic   = ENV['MQTT_TOPIC'] || params[:topic]
+  opt = {
+    host: ENV['MQTT_HOST'],
+    port: ENV['MQTT_PORT'].nil? ? 1883 : ENV['PORT'].to_i,
+    username: ENV['MQTT_USERNAME'],
+    password: ENV['MQTT_PASSWORD']
+  }
+  MQTT::Client.connect(opt) do |c|
+    c.publish(topic, message)
+  end
+end
